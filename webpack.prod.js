8051d4948f602af338b6706cafeeaf3e7732328a
ru39391 <dev@39391.ru>
2022-02-08 00:06:34 +0300

    The publicPath of output config was edited

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,9 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+  output: {
+    publicPath: '',
+  },
   module: {
     rules: [
       {
